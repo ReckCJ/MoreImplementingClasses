802ae8cbe7143ab8aae457e6dac5434385e16d49
reckcj <reckcj@rose-hulman.edu>
2017-12-18 20:33:23 -0500

    Committing after completion of m1

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -39,7 +39,7 @@ def main():
 
 
 # ----------------------------------------------------------------------
-# TODO: 2. Implement a class called   Baby   that has a constructor and
+# DONE: 2. Implement a class called   Baby   that has a constructor and
 #   two methods, as described below.  Your finished Baby class should
 #   cause the code above to display the expected output.  Hint: Your
 #   class will need instance variables that you must figure out.
@@ -90,11 +90,23 @@ def main():
 #
 ########################################################################
 class Baby(object):
-    
+    def __init__(self, string):
+        self.name = string
+        print('Hello baby ' + self.name + '!')
+        self.timepassed = 0
 
+    def feed_baby(self):
+        print('Thank you for feeding baby ' + self.name)
+        self.timepassed = 0
 
-
-
+    def hour_passes(self):
+        self.timepassed = self.timepassed + 1
+        if self.timepassed == 1:
+            print('Baby ' + self.name + ' is sleeping.')
+        elif self.timepassed == 2:
+            print('Baby ' + self.name + ' is awake. Time for food.')
+        else:
+            print('Baby ' + self.name + ' is CRYING uncontrollably! Feed the Baby!')
 
 
 # ----------------------------------------------------------------------
